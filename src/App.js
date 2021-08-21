7f57ce6b1cfb4cc8492585514eca291f6f7d51f2
Marcelckp <81308023+Marcelckp@users.noreply.github.com>
2021-08-21 21:08:26 +0200

    fix and set authentication for login in to the application
    will also fix the routes in app.js

--- src/App.js
+++ src/App.js
@@ -3,39 +3,53 @@ import HomeScreen from './screens/HomeScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import  {BrowserRouter, Route, Switch} from 'react-router-dom';
 import { auth } from './firebase';
+import { useDispatch, useSelector } from 'react-redux';
+import { logout, login, selectUser } from './features/userSlice';
 
 function App() {
-    const user=null;
+    const user = useSelector(selectUser) || null;
+    const dispatch = useDispatch()
 
     useEffect(() => {
         //this is a listener that will listen for firebase changes/ authenticated state change as done in SignInScreen.js so with this every time you refresh the authenticated state will be saved to your browser memory && returns a user auth call back
         const unsubscribe = auth.onAuthStateChanged( user => {
             if (user) {
+                //logged in
                 console.log(user);
-            } else {
+                                
+                dispatch(login({              //
+                    uid: user.uid,           //  redux login 
+                    email: user.email       //  payload
+                }))                        //
 
+            } else {
+                //log out
+                dispatch(logout);
             }
         })
-//equalivent to saying when it cleans up run the function ie)
-/**
- * return () => {
- *    unsubscribe
- * }
- * which saves computing power and boosts performance
-*/
+        //equalivent to saying when it cleans up run the function ie)
+        /**
+         * return () => {
+         *    unsubscribe
+         * }
+         * which saves computing power and boosts performance
+        */
         return unsubscribe
     },[])
+
+    console.log(user);
+
+
     return ( 
         <div className = "app">
             <BrowserRouter>
-                {/* {!user ? ( */}
-                    {/* <RegisterScreen /> */}
-                {/* ) : ( */}
-                    <Switch>
-                        <Route exact path='/home' render={(() => <HomeScreen />)} />
-                        <Route exact path='/' render={(() => <RegisterScreen />)} />
-                    </Switch>
-                {/* )}              */}
+                {!user ? (
+                    <RegisterScreen />
+                ) : (
+                        <Switch>
+                            <Route exact path='/' render={(() => <HomeScreen />)} />
+                        </Switch>
+                )}             
             </BrowserRouter>
         </div>
     );
