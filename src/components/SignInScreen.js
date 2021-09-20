0fd3fafdd93d1f4ad5c9404ebb8ff426467206af
Marcelckp <81308023+Marcelckp@users.noreply.github.com>
2021-09-20 14:53:43 +0200

    set up visual error messages for users trying to sign-in into the app

--- src/components/SignInScreen.js
+++ src/components/SignInScreen.js
@@ -1,37 +1,64 @@
-import React, {useRef} from 'react'
+import React, {useRef, useState} from 'react'
 import './SignInScreen.css';
+import RegisterScreen from '../screens/RegisterScreen';
 
 //explicit export
 import { auth } from '../firebase';
 
 function SignInScreen() {
 
+    const [loading, setLoading] = useState(false)
+    const [error, setErrors] = useState([])
+
     //react ref hook
     const emailRef = useRef(null);
     const passwordRef = useRef(null);
 
+
     const signIn = (e) => {
         e.preventDefault()
+        setLoading(true);
+        // if (!emailRef.current.value && !passwordRef.current.value) setErrors(['* Please enter a value for email & password']);
+        //     else if (!emailRef.current.value) setErrors(['* Please enter a email value']);
+        //     else if (!passwordRef.current.value) setErrors(['* Please enter a password value']);
+        //     else if (passwordRef.length < 6) setErrors(['* Password Value must be at least 6 characters long'])
         auth.signInWithEmailAndPassword(
             emailRef.current.value,
             passwordRef.current.value,
         ).then((res) => {
-            console.log(res)
+            console.log(res);
+            setLoading(false);
         }).catch((err) => {
-            alert(err.message)
+            console.log(err.message,err);
+            setLoading(false);
+
+            if (err.message === "There is no user record corresponding to this identifier. The user may have been deleted.") setErrors(['* No User was found try sign-in again']);
+            else if (err.message === 'The email address is badly formatted.') setErrors(['* Email has been badly formatted']);
+            else if (err.message === 'The password is invalid or the user does not have a password.') setErrors(['* Password has been badly formatted'])
         })
     }
 
-    return ( 
-        <div className="signInScreen">
-            <form>
-                <h1>Sign In</h1>
-                <input ref={emailRef} placeholder="Enter your email..." type='email' />
-                <input ref={passwordRef}placeholder='Enter your password' type="password" />
-                <button type="submit" onClick={signIn}>Sign In</button>
 
-                {/* <h4><span className='newToNet'>New to Netflix?  </span><span className='signUp4net' onClick={}>Sign up Now.</span></h4> */}
-            </form>
+    return ( 
+        
+        <div className="signInScreen">
+            {loading ? 
+                <div className="preloader"></div>
+            :
+            
+                <form>
+                    <h1>Sign In</h1>
+                    {error ? 
+                        <p className="error"> {error} </p>
+                    : null}
+                    <input ref={emailRef} placeholder="Enter your email..." type='email' />
+                    <input ref={passwordRef}placeholder='Enter your password' type="password" />
+                    <button type="submit" onClick={signIn}>Sign In</button>
+
+                    {/* <h4><span className='newToNet'>New to Netflix?  </span><span className='signUp4net' onClick={() =><RegisterScreen />}>Sign up Now.</span></h4> */}
+                </form>
+            
+            }     
         </div>
     )
 }
--- src/components/SignUpScreen.js
+++ src/components/SignUpScreen.js
@@ -1,35 +1,44 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import './SignInScreen.css'
 
 import { auth } from '../firebase';
 
 function SignUpScreen(props) {
 
+    const [loading, setLoading] = useState(false)
+
     const emailRef = useRef(null);
     const passwordRef = useRef(null);
 
     const register = (e) => {
         e.preventDefault();
+        setLoading(true);
         auth.createUserWithEmailAndPassword(
             emailRef.current.value,
             passwordRef.current.value,
         ).then((CreatedUser) => {
             console.log(CreatedUser)
+            setLoading(false);
         }).catch(error => {
             alert(error.message)
+            setLoading(false);
         })
     }
 
     return (
         <div className="signInScreen">
-            <form>
-                <h1>Sign Up</h1>
-                <input ref={emailRef} placeholder="Enter your email..." type='email' defaultValue={props.email} />
-                <input ref={passwordRef}placeholder='Enter your password' type="password" />
-                <button type="submit" onClick={register}>Sign Up</button>
+            {loading ? 
+                <p>Loading...</p>
+            :
+                <form>
+                    <h1>Sign Up</h1>
+                    <input ref={emailRef} placeholder="Enter your email..." type='email' defaultValue={props.email} />
+                    <input ref={passwordRef}placeholder='Enter your password' type="password" />
+                    <button type="submit" onClick={register}>Sign Up</button>
 
-                {/* <h4><span className='newToNet'>New to Netflix? </span><span className='signUp4net'>Sign up Now.</span></h4> */}
-            </form>
+                    {/* <h4><span className='newToNet'>New to Netflix? </span><span className='signUp4net'>Sign up Now.</span></h4> */}
+                </form>
+            }
         </div>
     )
 }
