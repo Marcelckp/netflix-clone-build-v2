28775ef5fb2ffa3cbd1996dcfbd0b1986e599829
Marcelckp <81308023+Marcelckp@users.noreply.github.com>
2021-09-24 00:23:33 +0200

    add code to aboutScreen Page and align the pages correctly

--- src/screens/AboutMeScreen/AboutScreen.js
+++ src/screens/AboutMeScreen/AboutScreen.js
@@ -18,7 +18,19 @@ function AboutScreen() {
                         </header>
             <div className="AboutScreen-container">
                 <h1>About Me</h1>
-                <p>Hi my names Marcel Palmer and I'm an aspiring developer</p>
+                <br />
+                <p>Hi my names Marcel Palmer and I'm an aspiring developer from South Africa.</p>
+                
+                <p>lsdfgs dlfasdgieh wpdfgj sodifjgsop fjhj'a;sdfp aosdjfjkjkjab cdefjo asdf alsdfj aeogih a df a b c d e f g h ij k lm n o p q rs t u v w x y z</p>
+                <br />
+                <h2>Contact Me</h2>
+                <br />
+                <form className='contactMe-form'>
+                    <input type="text" placeholder='Please enter your name here...' />
+
+                    <textarea placeholder='Please enter your message here...'/>
+                    <button /*type="submit" */>Send</button>
+                </form>
             </div>
         </div>
     )
