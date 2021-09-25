e776944955f8606b025a173bca0d6fb9f426061f
Marcelckp <81308023+Marcelckp@users.noreply.github.com>
2021-09-25 16:30:57 +0200

    allow the about contact form to send emails to the configured email address

--- src/screens/AboutMeScreen/AboutScreen.js
+++ src/screens/AboutMeScreen/AboutScreen.js
@@ -1,23 +1,44 @@
 import React, { useState } from 'react';
-import './AboutScreen.css'
+import './AboutScreen.css';
+import emailjs from 'emailjs-com';
+
+//components
 import Nav from '../../components/Nav';
 import PhotoMe from '../../img/IMG_3848.jpeg'
 import success from '../../img/success.png'
 
 function AboutScreen() {
 
-    const [submitted, setSubmitted] = useState(false);
+    const [loading, setLoading] = useState(true);
 
-    // const [messageAgain, setMessageAgain] = useState(false);
+    const [successful, setSuccessful] = useState(false);
+
+    const [failed, setFailed] = useState(false);
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        setSubmitted(true);
+        setLoading(true);
+        emailjs.sendForm('service_iv9rirp','template_h8ow76s', e.target, 'user_NLpfJjcfCiqUZv0zspDly')
+            .then((response) => {
+                setLoading(false);
+                setSuccessful(true);
+                    console.log('successful');
+                }, error => {
+                    console.log(error);
+                    setFailed(true);
+            })
+            .catch((error) => {
+                console.log(error);
+                setFailed(true);
+            })
+            e.target.reset()
     }
 
     const messageAgain = (e) => {
         e.preventDefault();
-        setSubmitted(false);
+        setLoading(false);
+        setSuccessful(false);
+        setFailed(false);
     }
 
     return (
@@ -39,8 +60,9 @@ function AboutScreen() {
                 <h2>Contact Me</h2>
                 <br />
                 
-                {submitted ? 
-                        <div className="fadein popup-aboutScreen">
+                {loading ?  <div className="preloader center-email-block"></div> :
+
+                    !loading && successful ? <div className="fadein popup-aboutScreen">
                             <img src={success} alt="successful" className="success-icon-aboutScreen" />
                             <div className="title-aboutScreen">
                                 <p>Success!</p>
@@ -54,17 +76,17 @@ function AboutScreen() {
                                 </button>
                             </div>
                         </div>
-                    :
-                        <form /*action='#'*/ className='contactMe-form' /*method='post'*/ >
-                            <label htmlFor="nameAboutScreen">Name</label>
-                            <input type="text" id='nameAboutScreen' required placeholder='Enter Your Name' />
+                    : !loading && failed ? <div className="failed">Message failed to send </div> :
+                        <form className='contactMe-form' onSubmit={handleSubmit}>
+                            <label htmlFor="nameAbout">Name</label>
+                            <input type="text" id='nameAbout' name='fullName' required placeholder='Enter Your Name' />
                             
-                            <label htmlFor="emailAboutScreen">Email</label>
-                            <input type="email" required placeholder='Enter Your Email' id='emailAboutScreen'/>
+                            <label htmlFor="emailAbout">Email</label>
+                            <input type="email" name='email' required placeholder='Enter Your Email' id='emailAbout'/>
 
-                            <label htmlFor="messageAboutScreen">Message</label>
-                            <textarea required cols='30' rows='10' id='messageAboutScreen'/>
-                            <button onClick={handleSubmit}>Submit</button>
+                            <label htmlFor="messageAbout">Message</label>
+                            <textarea required cols='30' rows='10' name='message' id='MessageAbout'/>
+                            <button type='submit'>Submit</button>
                         </form>
                 }
             </div>
